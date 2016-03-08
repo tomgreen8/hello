8ac9c860486b6686ba7592e6550d9bfe537dd2a0
liuquan <liuquan@youku.com>
2016-03-08 14:48:59 +0800

    提交

--- src/com/personal/hello/Main.java
+++ src/com/personal/hello/Main.java
@@ -3,11 +3,13 @@ package com.personal.hello;
 public class Main {
 	
 	public static void main(String[] args) {
-		fdsfsf
-		System.out.println("mainhellotinaf");
-		//billgredfen9进行了编辑
-		nnikjjdfffdfd
-		dsfsf
+		//tom8提交///
+		System.out.println("hejglo");
+		//billgreen9进行了编辑
+		for (int i = 0; i < args.length; i++) {
+			
+		}
+
 	}
 
 }
