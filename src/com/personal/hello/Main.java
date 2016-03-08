604ac79513c5992a8d1da3cb9497c2ee15644968
liuquan <liuquan@youku.com>
2016-03-08 15:50:22 +0800

    提交

--- src/com/personal/hello/Main.java
+++ src/com/personal/hello/Main.java
@@ -10,17 +10,14 @@ public class Main {
 		System.out.println("hejglo");
 		// billgreen915:41进行了jjdf辑 14:50 xingagi
 		for (int i = 0; i < args.length; i++) {// 15:23 15:24
-
 			// ////
-
 			// fdfdf
-
 		}
 
 		Map<String, Object> map = new HashMap<>();
 		map.put("13312", 12321);
 
-		// dffdkjf 15:41
+		// dffdkjf 15:41///
 
 	}
 
