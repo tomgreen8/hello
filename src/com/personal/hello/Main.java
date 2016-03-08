b980bdbdb69e7cfdcfab7573c809abbed2c6885e
liuquan <liuquan@youku.com>
2016-03-08 15:43:16 +0800

    嗲吗提交

--- src/com/personal/hello/Main.java
+++ src/com/personal/hello/Main.java
@@ -1,5 +1,8 @@
 package com.personal.hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 	
 	public static void main(String[] args) {
@@ -7,8 +10,12 @@ public class Main {
 		System.out.println("hejglo");
 		//billgreen9进行了编辑 14:50 xingagi
 		for (int i = 0; i < args.length; i++) {//15:23 15:24
-			//
+			//////
 		}
+		
+		Map<String,Object> map = new HashMap<>();
+		map.put("13312", 12321);
+		
 
 	}
 
--- src/com/personal/hello/Mem.java
+++ src/com/personal/hello/Mem.java
@@ -8,5 +8,5 @@ package com.personal.hello;
  *
  */
 public class Mem {
-
+///
 }
