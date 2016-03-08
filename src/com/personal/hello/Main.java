7e0868e8d92e5181f34f2f10695c863f5d9fe22c
billgreen9 <liuquan899@qq.com>
2016-03-08 14:52:11 +0800

    Update Main.java

--- src/com/personal/hello/Main.java
+++ src/com/personal/hello/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		//tom8提交///
 		System.out.println("hejglo");
-		//billgreen9进行了编辑 14:50
+		//billgreen9进行了编辑 14:50 xingagi
 		for (int i = 0; i < args.length; i++) {
 			
 		}
